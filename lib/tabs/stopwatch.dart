826c741974fd195145df524a011751e0ceee8ecb
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-02-13 14:03:07 +0500

    Stopwatch functions done

--- lib/tabs/stopwatch.dart
+++ lib/tabs/stopwatch.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:stop_watch_timer/stop_watch_timer.dart';
 
 class StopWatch extends StatefulWidget {
   const StopWatch({Key? key}) : super(key: key);
@@ -9,11 +10,66 @@ class StopWatch extends StatefulWidget {
 }
 
 class _StopWatchState extends State<StopWatch> {
-  final StopWatchTimer _stopWatchTimer = StopWatchTimer();
+  int seconds = 0, minutes = 0, hours = 0;
+  String digitSeconds = "00", digitMinutes = "00", digitHours = "00";
+  Timer? timer;
+  bool started = false;
+  List laps = [];
+
+  void stop() {
+    timer!.cancel();
+    setState(() {
+      started = false;
+    });
+  }
+
+  void reset() {
+    timer!.cancel();
+    setState(() {
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+      digitHours = "00";
+      digitMinutes = "00";
+      digitSeconds = "00";
+      started = false;
+    });
+  }
+
+  void addLaps() {
+    String lap = "$digitHours:$digitMinutes:$digitSeconds";
+    setState(() {
+      laps.add(lap);
+    });
+  }
+
+  void start() {
+    started = true;
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      int localSeconds = seconds + 1;
+      int localMinutes = minutes;
+      int localHours = hours;
+      if (localSeconds > 59) {
+        localMinutes++;
+        localSeconds = 0;
+        if (localMinutes > 59) {
+          localHours++;
+          localMinutes = 0;
+        }
+      }
+      setState(() {
+        seconds = localSeconds;
+        minutes = localMinutes;
+        hours = localHours;
+        digitSeconds = (seconds > 10) ? "$seconds" : "0$seconds";
+        digitMinutes = (minutes > 10) ? "$minutes" : "0$minutes";
+        digitHours = (hours > 10) ? "$hours" : "0$hours";
+      });
+    });
+  }
 
   @override
   void dispose() {
-    _stopWatchTimer.dispose();
     super.dispose();
   }
 
