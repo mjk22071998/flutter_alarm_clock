38c5f012c614b8f5d5caa6cc97de2df488b79aa2
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-05 15:04:38 +0500

    Buttons Added

--- lib/tabs/timer.dart
+++ lib/tabs/timer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CountdownTimer extends StatefulWidget {
@@ -7,12 +9,17 @@ class CountdownTimer extends StatefulWidget {
   _CountdownTimerState createState() => _CountdownTimerState();
 }
 
-class _CountdownTimerState extends State<CountdownTimer> {
+class _CountdownTimerState extends State<CountdownTimer>
+    with TickerProviderStateMixin {
+  bool started = false;
+
+  late AnimationController controller;
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        Expanded(
+      children: [
+        const Expanded(
           child: Center(
             child: Text(
               "00:00:00",
@@ -23,6 +30,49 @@ class _CountdownTimerState extends State<CountdownTimer> {
             ),
           ),
         ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Text((started) ? "Pause" : "Start"),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: const StadiumBorder(),
+                      elevation: 5,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "Stop",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.tealAccent,
+                      shape: const StadiumBorder(),
+                      elevation: 5,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
       ],
     );
   }
